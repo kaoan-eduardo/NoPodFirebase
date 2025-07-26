d6e224c492e27243be5d4442f339b1f04758f2d5
kaoan-eduardo <kaoan.eduardo@hotmail.com>
2025-07-26 19:23:53 -0300

    Mudança de API

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,21 +1,34 @@
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+interface ExpoExtra {
+  EXPO_PUBLIC_API_KEY: string;
+  EXPO_PUBLIC_AUTH_DOMAIN: string;
+  EXPO_PUBLIC_PROJECT_ID: string;
+  EXPO_PUBLIC_STORAGE_BUCKET: string;
+  EXPO_PUBLIC_MESSAGING_SENDER_ID: string;
+  EXPO_PUBLIC_APP_ID: string;
+}
+
+const expoConfig = Constants.expoConfig ?? { extra: {} as ExpoExtra };
+const extra = expoConfig.extra as ExpoExtra;
+
 const firebaseConfig = {
-  apiKey: "REMOVIDO",
-  authDomain: "nopodfirebase-b02d6.firebaseapp.com",
-  projectId: "nopodfirebase-b02d6",
-  storageBucket: "nopodfirebase-b02d6.appspot.com",
-  messagingSenderId: "35048749950",
-  appId: "1:35048749950:web:d159bef41000a28052912f",
+  apiKey: extra.EXPO_PUBLIC_API_KEY ?? "",
+  authDomain: extra.EXPO_PUBLIC_AUTH_DOMAIN ?? "",
+  projectId: extra.EXPO_PUBLIC_PROJECT_ID ?? "",
+  storageBucket: extra.EXPO_PUBLIC_STORAGE_BUCKET ?? "",
+  messagingSenderId: extra.EXPO_PUBLIC_MESSAGING_SENDER_ID ?? "",
+  appId: extra.EXPO_PUBLIC_APP_ID ?? "",
 };
 
-// Inicializa o app Firebase
+console.log("🔥 Firebase Config:", firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 
-// Funciona no celular e no navegador
 const auth = getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
